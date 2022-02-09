a95f93cd28fb9132ac37e401ebf81f1fdb00f005
Rob685 <arevalo@princeton.edu>
2022-02-08 21:36:46 -0500

    changes to lnlike for b

--- ln_like_sampler_b.py
+++ ln_like_sampler_b.py
@@ -41,29 +41,6 @@ nobs = 2
 obs_err = np.array([0.0001, 0.0005])
 obs = np.array([1.503, 1.503])
 
-# last TESS observation:
-t_tess = 4664.65
-
-# last K2 observation:
-t_K2 = 2265
-
-t = t_tess - t_K2
-
-t_orbits = t/8.24958
-
-t_orb = t_orbits*5.98
-tmax = 1.0*t_orb
-Nout = 200
-
-t = np.linspace(0,tmax,Nout)
-obs_tess = np.where(np.round(abs(t - t_orb),6) == np.round(np.min(abs(t - t_orb)),6))[0][0]
-delta_index = 0
-obs_idx=[delta_index,obs_tess+delta_index]
-
-nobs = 2
-obs_err = np.array([0.0001, 0.0005])
-obs = np.array([1.503, 1.503])
-
 def makesim(theta):
 
 
@@ -111,50 +88,51 @@ def run(sim):
 
 def compute_model(theta):
 
-    if theta.ndim > 1:
-        sims = [makesim(param) for param in theta]
+    #if theta.ndim > 1:
+    #    sims = [makesim(param) for param in theta]
+#
+    #    period_ratios = [run(sim) for sim in sims]
+#
+#
+#
+    #    return [periods[obs_idx] for periods in period_ratios]
 
-        period_ratios = [run(sim) for sim in sims]
+    #else:
+    sim = makesim(theta)
+    period_ratios = run(sim)
+    return period_ratios[obs_idx]
 
+def gen_priors_array(seed):
 
-
-        return [periods[obs_idx] for periods in period_ratios]
-
-    else:
-        sim = makesim(theta)
-        period_ratios = run(sim)
-        return period_ratios[obs_idx]
-
-def gen_priors_array(seed, n):
-
-    size_arr = int(n)
+    #size_arr = int(n)
 
     rng = np.random.default_rng(seed)
 
-    eforced_0 = rng.uniform(0.0005,0.23,size=size_arr) # changing the upper limit to the crossing eccentricity
-    efree_0 = rng.uniform(0.0005,0.003,size=size_arr) # changing the bounds because experiments show that
-    mu_0 = rng.uniform(np.log10(8.964e-6), np.log10(5.25e-4), size=size_arr) # new upper limit of 0.55 Mjup by Mascareno
+    eforced_0 = float(rng.uniform(0.0005,0.23,size=1)) # changing the upper limit to the crossing eccentricity
+    efree_0 = float(rng.uniform(0.0005,0.003,size=1)) # changing the bounds because experiments show that
+    mu_0 = float(rng.uniform(np.log10(8.964e-6), np.log10(5.25e-4), size=1)) # new upper limit of 0.55 Mjup by Mascareno
 
-    mb_0 = stats.norm.rvs(loc=0.69, scale=0.19, size=size_arr)
+    #mb_0 = stats.norm.rvs(loc=0.69, scale=0.19, size=size_arr)*9.54e-4
+    mb_0 = float(stats.truncnorm.rvs(a=(0 - 0.69)/0.19, b = np.inf, loc=0.69, scale=0.19, size=1))
+    #eb_0 = stats.norm.rvs(loc=0.13, scale=0.07, size=size_arr)
+    eb_0 = float(stats.truncnorm.rvs(a = (0 - 0.13)/0.07, b = np.inf, loc=0.13, scale=0.07, size=1))
+    pomegab_0 = float(np.random.uniform(0, 2*np.pi, size=1))
 
-    eb_0 = stats.norm.rvs(loc=0.13, scale=0.07, size=size_arr)
+    thetab_0 = float(np.random.uniform(0, 2*np.pi, size=1))
 
-    pomegab_0 = np.random.uniform(0, 2*np.pi, size=size_arr)
+    deltaT_0 = float(rng.uniform(0, 2000, size=1))
 
-    thetab_0 = np.random.uniform(0, 2*np.pi, size=size_arr)
+    #prior_set = np.column_stack((eforced_0, efree_0, mu_0, deltaT_0, mb_0, eb_0, pomegab_0, thetab_0))
+    prior_set = (eforced_0, efree_0, mu_0, deltaT_0, mb_0, eb_0, pomegab_0, thetab_0)
+    #valid_priors = []
+    #for param in prior_set:
+    #    if param[4] < 0 or param[5] < 0:
+    #        continue
+    #    else:
+    #        valid_priors.append(param)
 
-    deltaT_0 = rng.uniform(0, 2000, size=size_arr)
+    return prior_set
 
-    prior_set = np.column_stack((eforced_0, efree_0, mu_0, deltaT_0, mb_0, eb_0, pomegab_0, thetab_0))
-
-    valid_priors = []
-    for param in prior_set:
-        if param[4] < 0 or param[5] < 0:
-            continue
-        else:
-            valid_priors.append(param)
-
-    return valid_priors
 
 def lnlike(theta):
 
@@ -162,42 +140,49 @@ def lnlike(theta):
 
     model = compute_model(theta)
     argument = (obs - model)**2 / obs_err**2
-    if theta.ndim > 1:
-        loglike = [0.5*np.sum(arg) for arg in argument]
-    else:
-        loglike = 0.5*np.sum(argument)
+    #if theta.ndim > 1:
+    #    loglike = [0.5*np.sum(arg) for arg in argument]
+    #else:
+    loglike = 0.5*np.sum(argument)
     return loglike
 
-def get_posteriors(prior):
+def get_posteriors(seed):
 
-    like_val = lnlike(prior)
+    priors = gen_priors_array(seed)
+    like_val = lnlike(priors)
 
-    rng = np.random.default_rng(42)
+    rng = np.random.default_rng(seed)
 
-    prob = np.log10(rng.random())
+    prob = np.log10(np.random.random())
+
 
     if -like_val > prob:
         post = prior
 
         return post
+
     else:
     	return np.nan
 
 
-priors = gen_priors_array(42, sys.argv[1])
-#seeds = list(range(int(sys.argv[1]),int(sys.argv[2])))
+
+seeds = list(range(int(sys.argv[1]),int(sys.argv[2])))
+
+n = int(1e4)
+
+seed_batches = [seeds[i:i + n] for i in range(0, len(seeds), n)]
 
 
 #seed_batches = [seeds[i:i + n] for i in range(0, len(seeds), n)]
-start_time = timeit.default_timer()
-#for i,seed_batch in enumerate(seed_batches):
-if __name__ == '__main__':
-    #pool = Pool()
-    #results = pool.map(get_posteriors,priors)
-    results = p_map(get_posteriors,priors)
-    #pool.close()
-    #pool.join()
-    #np.save('/Users/Helios/gdrive_pu/tamayo_research/lnlike_100mil/batch_{}.npy'.format(i+576), results) # 25 mil broke after file 257 so replacing format so it doesn't overwrite
-    print("--- %s seconds ---" % (timeit.default_timer() - start_time))
+# start_time = timeit.default_timer()
+for i,seed_batch in enumerate(seed_batches):
+    if __name__ == '__main__':
+        #pool = Pool()
+        #results = pool.map(get_posteriors,priors)
+        results = p_map(get_posteriors,seed_batch)
+        #pool.close()
+        #pool.join()
+        #np.save('/Users/Helios/gdrive_pu/tamayo_research/lnlike_100mil/batch_{}.npy'.format(i+576), results) # 25 mil broke after file 257 so replacing format so it doesn't overwrite
+        #print("--- %s seconds ---" % (timeit.default_timer() - start_time))
 
-    print(len(np.array(results)[~np.isnan(results)]))
+        print(len(np.array(results)[~np.isnan(results)]))
